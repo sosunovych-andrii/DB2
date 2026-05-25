9e77f4caf2c37f7b59cb49a3bf2d64b73665caae
Andrii Sosunovych <sosunovych.andrii@gmail.com>
2026-05-25 20:21:59 +0300

    Update routers

--- src/main.py
+++ src/main.py
@@ -8,6 +8,8 @@ from src.routers.product_types import router as product_types_router
 from src.routers.auth import router as users_router
 from src.routers.mk1 import router as mk1_router
 from src.routers.lab4 import router as lab4_router
+from src.routers.lab5 import router as lab5_router
+from src.routers.mk2 import router as mk2_router
 
 
 app = FastAPI()
@@ -21,3 +23,5 @@ app.include_router(router=product_types_router)
 app.include_router(router=users_router)
 app.include_router(router=mk1_router)
 app.include_router(router=lab4_router)
+app.include_router(router=lab5_router)
+app.include_router(router=mk2_router)
